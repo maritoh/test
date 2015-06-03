062e497808843b3de8fc323c0d4281b78f4e53fa
marioh <marito.h@gmail.com>
2015-06-02 20:11:44 -0600

    Agregar la lectura analógica.

--- test/test.ino
+++ test/test.ino
@@ -1,10 +1,19 @@
 int x;
+int val=0;
+int analogPin = A3;
 void setup()
 {
   x =0;
+   Serial.begin(9600);          //  setup serial
 }
 
 void loop()
 {
   x++;
+}
+
+void lectura_analogica(){
+ val = analogRead(analogPin);    // read the input pin
+  Serial.println(val);             // debug value
+ 
 }
